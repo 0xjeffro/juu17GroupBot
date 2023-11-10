7f8a1eea6aa52eb19adbe0137bb996680ae8aa88
Jeffro Tanvik <00002@protonmail.com>
2023-11-11 04:19:13 +0800

    update

--- main.go
+++ main.go
@@ -244,7 +244,7 @@ func testResultHandler(c *gin.Context) {
 	}
 	if !req.Pass {
 		// 把用户移出群组
-		until := time.Now().Add(time.Minute * 180).Unix()
+		until := time.Now().Add(time.Hour * 6).Unix()
 		actions.BanUser(bot, CurrentChatIDInt64, req.UserID, until)
 	} else {
 		// 把用户解除禁言
